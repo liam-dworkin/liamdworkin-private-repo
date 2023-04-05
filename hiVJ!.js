e877fc1d257eac1afacb140babbb84a6d7d2b978
liam-dworkin <124173727+liam-dworkin@users.noreply.github.com>
2023-04-05 09:39:31 -0400

    Create hiVJ!.js
    First commit of my "Hi VJ" file.

---
+++ hiVJ!.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  strokeWeight(1.5);
+  fill("rgb(18,224,255)");
+
+  textFont("Helvetica");
+  textSize(45);
+  text("HI VJ!", 100, 70);
+ 
+}
